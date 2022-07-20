58184d5c8b664728fd6d3ba7577bc651f7207dd7
Amaan Zahid <amaanrajput302@gmail.com>
2022-07-20 17:54:25 +0500

    moved projects to seperate folders

--- projects/frontend-calculator/README.md
+++
@@ -1,2 +0,0 @@
-# frontend-calculator
-HTML, CSS, JS frontend Calculator Project. Created it to practice basics. 
--- projects/frontend-my-todos/assets/js/store.js
+++
@@ -1,9 +0,0 @@
-class Store {
-  static getItem(key) {
-    return JSON.parse(localStorage.getItem(key)) || [];
-  }
-
-  static setItem(key, val) {
-    localStorage.setItem(key, JSON.stringify(val));
-  }
-}
